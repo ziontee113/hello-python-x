028ee6921ed47c4a116502483240342ce2c046b5
ziontee113 <ziontee113@gmail.com>
2023-10-26 09:25:09 +0700

    Add some notes to test notebook

--- notebooks/test.ju.py
+++ notebooks/test.ju.py
@@ -1,3 +1,11 @@
+# %%%[md]
+
+## Hello Jupyter Notebook from Neovim
+
+- Edit Jupyter Notebook in Neovim via [jupynium.nvim](https://github.com/kiyoon/jupynium.nvim)
+- Environment inside `poetry shell`
+- Dark mode using `jupyterthemes`
+
 # %%
 
 import numpy as np
@@ -11,4 +19,5 @@ print("hello world")
 
 x = np.linspace(-10, 10, 1000)
 y = np.sin(x**2)
+
 plt.plot(x, y)
